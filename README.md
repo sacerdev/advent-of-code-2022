5449a815b8a93ba7f984355c28ad7f71c52ad5d0
sacerdev <118774585+sacerdev@users.noreply.github.com>
2022-12-01 11:40:32 +0000

    day1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # advent-of-code-2022
+
+## Times
+
+### Day1
+- Part1: 10:45
+- Part2: 13:45
--- day1/index.js
+++ day1/index.js
@@ -2,3 +2,26 @@ const fs = require('fs');
 
 const data = fs.readFileSync('./day1/input.txt', { encoding: 'utf8', flag: 'r' });
 const lines = data.split(/\r?\n/);
+
+const sumCals = [];
+let sum = 0;
+
+lines.forEach((l, i) => {
+    if (l?.length > 0) {
+        sum += parseInt(l);
+    } else if (i > 0) {
+        sumCals.push(sum);
+        sum = 0;
+    }
+});
+
+console.log('Part1: ', Math.max(...sumCals));
+
+const fatElv1 = Math.max(...sumCals);
+sumCals.splice(sumCals.indexOf(fatElv1), 1);
+const fatElv2 = Math.max(...sumCals);
+sumCals.splice(sumCals.indexOf(fatElv2), 1);
+const fatElv3 = Math.max(...sumCals);
+sumCals.splice(sumCals.indexOf(fatElv3), 1);
+
+console.log('Part2: ', fatElv1 + fatElv2 + fatElv3);
